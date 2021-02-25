4e41ac58d223caf7dd2ba4cc8de3381567c64094
Natalia Boichuk <boichuk.n.m@gmail.com>
2021-02-25 18:28:32 +0200

    add task about check if its a sorted array and how

---
+++ Loops/order.py
@@ -0,0 +1,31 @@
+# Write a program that given an array of integers determines if it is sorted in "ascending" order,
+# "descending" order or "not sorted" at all.
+#
+# Example
+#
+# For a = [10, 5, 4], the output should be
+# order(a) = "descending";
+# For a = [6, 20, 160, 420], the output should be
+# order(a) = "ascending";
+# For a = [1, 7, 0, 4, 8, 1], the output should be
+# order(a) = "not sorted".
+# [input] array.integer a
+#
+# 1 < a.length < 100, all of numbers are different
+#
+# [output] string:  "ascending", "descending" or "not sorted".
+
+def order(a):
+    """
+
+    :param a: list
+    :return: str
+    """
+    sort_a = sorted(a)
+    revers_sort_a = sort_a[::-1]
+    if a == sort_a:
+        return 'ascending'
+    elif a == revers_sort_a:
+        return 'descending'
+    else:
+        return 'not sorted'
