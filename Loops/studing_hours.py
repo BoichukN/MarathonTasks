39354c5fd0f0fc003994b1474f692c2cba43897c
Natalia Boichuk <boichuk.n.m@gmail.com>
2021-02-25 18:53:38 +0200

    add task about max non-decreasing subarray

---
+++ Loops/studing_hours.py
@@ -0,0 +1,28 @@
+# 's3ooOOooDy' has exams. He wants to study hard this time.
+# He has an array of studying hours per day for the previous exams.
+# He wants to know the length of the maximum non-decreasing contiguous subarray of the studying days,
+# to study as much before his current exams.
+#
+# Example:
+#
+# For a = [2,2,1,3,4,1] the answer is 3.
+#
+# [input] array.integer a
+#
+# The number of hours he studied each day.
+#
+# [output] integer
+#
+# The length of the maximum non-decreasing contiguous subarray.
+
+def studying_hours(a):
+    max_len = []
+    m = 1
+    for i in range(1, len(a)):
+        if a[i] >= a[i-1]:
+            m += 1
+        else:
+            max_len.append(m)
+            m = 1
+    max_len.append(m)
+    return max(max_len)
